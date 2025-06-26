bacc467b10db472247e94d02f7c178f4a27681b1
pkuimyy <pkuimyy@outlook.com>
2025-06-26 16:21:27 +0800

    feat: initialize echolog project (#1)
    * feat: update doc
    
    * feat: update package-lock.json
    
    * feat: update doc

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-## echolog
+# echolog
+
+![version](https://img.shields.io/github/package-json/v/pkuimyy/echolog?style=flat-square)
+
+export your favorite music albums list.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <main>Hello, PKUIMUYY!</main>;
+  return (
+    <main>
+      <Image src="/echolog.svg" alt="echolog" width={100} height={100}/>
+            this is echolog!
+    </main>
+  )
+  ;
 }
